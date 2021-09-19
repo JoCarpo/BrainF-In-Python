072db84f92e28a5602f3f643d2516c0e8d92b923
JoCarpo <81490409+JoCarpo@users.noreply.github.com>
2021-09-19 10:13:50 +1000

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from compiler import run, lexer, lstNum
+import sys
+
+code = sys.argv[1]
+
+with open(code, 'r') as file:
+	lexed, loops = lexer(file)
+
+cells, currentCell = run(lexed, loops)
+cells = lstNum(cells, currentCell)
+print(f'\n\033[1;32m=> {cells}\033[0m')
