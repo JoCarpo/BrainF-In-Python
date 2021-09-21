af3f75e9c6e6a1ca4c146e110b500108ce2fa6a4
MrLuck <81490409+MisterrLuck@users.noreply.github.com>
2021-09-21 10:50:12 +1000

    Update build.sh

--- build/linux/build.sh
+++ build/linux/build.sh
@@ -1,1 +1,13 @@
+dir=$(pwd)
 
+cp brainf.sh ~/
+cd ../../
+cp -r src ~/
+cd ~/
+mv src .brainf
+mv brainf.sh .brainf.sh
+echo "source ~/.brainf.sh" >> .bashrc
+
+echo Type 'brain' followed by your file name, to run BrainF***
+
+cd dir
